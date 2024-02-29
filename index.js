8f999b31651a3a94a83c9c4a8d3f72a40b928b6c
bimobaskoro <bimoappbaskoro@gmail.com>
2024-02-29 10:59:46 +0700

    lalayeye

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log("Hello world");
 console.log("New World");
+console.log("New World");
+console.log("New World");
+console.log("New World");
+console.log("New World");
+console.log("New World");
+console.log("New World");
